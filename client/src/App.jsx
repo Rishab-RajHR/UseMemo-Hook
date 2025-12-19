f1da2d809db9417405fea3b56946c518d619f94b
Rishab Raj <rr0948555@gmail.com>
2025-12-19 14:50:52 +0530

    UseMemo Hook

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseMemoHook from './UseMemoHook'
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is App.jsx file </h2>
+      <UseMemoHook />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/UseMemoHook.jsx
@@ -0,0 +1,23 @@
+import React, { useMemo, useState } from 'react'
+
+const UseMemoHook = () => {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(2);
+  
+  const multiply = useMemo(function multi() {
+      console.log("Multi Function For Count State")
+      return count * 2;
+  }, [count])
+  return (
+    <>
+      <h2>UseMemo Hook in ReactJS</h2>
+      <h3>Count: {count} </h3>
+      <h3>{multiply}</h3>
+      <button onClick={()=> setCount(count+1)}>Update Count</button>
+      <h3>Data: {data} </h3>
+      <button onClick={()=> setData(data*10)}>Update Data</button>
+    </>
+  )
+}
+
+export default UseMemoHook
